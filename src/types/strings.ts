52b0dede50c34c7d2869724d97376ecf2dca2df7
Amit <amittur71@gmail.com>
2021-05-31 14:07:31 +0300

    update autoCompleteURL over https

--- src/types/strings.ts
+++ src/types/strings.ts
@@ -1,6 +1,6 @@
 export const locationURL = 'https://dataservice.accuweather.com/locations/v1/search?';
 export const forecastURL = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey?';
-export const autoCompleteURL = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
+export const autoCompleteURL = 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete';
 
 export const openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?";
 
